5f18818f6643d9c60085476bd6be700b6167b06f
RadAd <adamgates84+github@gmail.com>
2020-08-26 12:35:18 +1000

    Fix Init.ps1

--- Init.ps1
+++ Init.ps1
@@ -1,19 +1,27 @@
-$project = "Foo"
-[string] $rootPathForFiles = "."
-[string[]] $listOfFilesToRename = Get-ChildItem -Path $rootPathForFiles -File
-$listOfFilesToRename | ForEach-Object {
-    if ($_ -ne "Init.ps1") {
-        $oldContent = Get-Content -Path $_
-        $newContent = $oldContent
-        $newContent = $newContent -replace "`$project", "$project"
-        if ($newContent -ne $oldContent) {
-            $newContent | Set-Content -Path $_
-        }
+$project = $args[0]
+if ($project -eq $null) {
+    Write-Output "Init.ps1 [project]"
+}
+else {
+    [string] $rootPathForFiles = "."
+    [string[]] $listOfFilesToRename = Get-ChildItem -Path $rootPathForFiles -File
+    $listOfFilesToRename | ForEach-Object {
+        if ($_ -ne "Init.ps1") {
+            $oldContent = Get-Content -Path $_
+            $newContent = $oldContent
+            $newContent = $newContent -replace "\`$project", "$project"
+            if ($newContent -ne $oldContent) {
+                Write-Output "Replace content $_"
+                $newContent | Set-Content -Path $_
+            }
 
-        $newName = $_
-        $newName = $newName -replace "`$project", "$project"
-        if ($newName -ne $_) {
-            Rename-Item -Path $_ -NewName $newName
+            $newName = $_
+            $newName = $newName -replace "\`$project", "$project"
+            #Write-Output "Rename-Item -Path $_ -NewName $newName"
+            if ($newName -ne $_) {
+                Write-Output "Rename $_ -> $newName"
+                Rename-Item -Path $_ -NewName $newName
+            }
         }
     }
 }
