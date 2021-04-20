0ee702ef59a1d643f2b5b8feaaae0a72516ab2be
RadAd <adamgates84+github@gmail.com>
2021-04-20 11:24:09 +1000

    The IDE will generate necessary GUIDs

--- Init.ps1
+++ Init.ps1
@@ -6,15 +6,15 @@ if ($project -eq $null) {
 else {
     [string] $rootPathForFiles = "."
     [string[]] $listOfFilesToRename = Get-ChildItem -Path $rootPathForFiles -File
-    [string] $guid1 = [GUID]::NewGUID().ToString('B')
-    [string] $guid2 = [GUID]::NewGUID().ToString('B')
+    # [string] $guid1 = [GUID]::NewGUID().ToString('B')
+    # [string] $guid2 = [GUID]::NewGUID().ToString('B')
     $listOfFilesToRename | ForEach-Object {
         if ($_ -ne $this) {
             $oldContent = Get-Content -Path $_
             $newContent = $oldContent
             $newContent = $newContent -replace "\`$project", $project
-            $newContent = $newContent -replace "{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}", $guid1
-            $newContent = $newContent -replace "{B55B0C97-2A14-4076-9174-6A137134AF51}", $guid2
+            # $newContent = $newContent -replace "{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}", $guid1
+            # $newContent = $newContent -replace "{B55B0C97-2A14-4076-9174-6A137134AF51}", $guid2
             if ($newContent -ne $oldContent) {
                 Write-Output "Replace content $_"
                 $newContent | Set-Content -Path $_
