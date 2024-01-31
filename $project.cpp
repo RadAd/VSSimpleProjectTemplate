c0ace8dbb842cd1056f1f68930b0a527c5611aa7
RadAd <adamgates84+github@gmail.com>
2024-02-01 09:27:21 +1100

    Change to use spaces

--- $project.cpp
+++ $project.cpp
@@ -6,8 +6,8 @@
 
 int _tmain(int argc, const TCHAR* const argv[])
 {
-	_tprintf(_T("Hello World\n"));
-	return EXIT_SUCCESS;
+    _tprintf(_T("Hello World\n"));
+    return EXIT_SUCCESS;
 }
 #endif
 
@@ -17,7 +17,7 @@ int _tmain(int argc, const TCHAR* const argv[])
 
 int WINAPI _tWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nShowCmd)
 {
-	MessageBox(NULL, _T("Hello World"), _T("$project"), MB_OK | MB_ICONINFORMATION);
-	return 0;
+    MessageBox(NULL, _T("Hello World"), _T("$project"), MB_OK | MB_ICONINFORMATION);
+    return 0;
 }
 #endif
