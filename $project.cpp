c1bc06b6860a5bff4cfa7f34df1eacf3d2c5722e
RadAd <adamgates84+github@gmail.com>
2026-04-13 17:05:59 +1000

    Change lpCmdLine type from LPCTSTR to LPCSTR

--- $project.cpp
+++ $project.cpp
@@ -15,7 +15,7 @@ int _tmain(const int argc, const TCHAR* const argv[])
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
-int WINAPI _tWinMain(_In_ const HINSTANCE hInstance, _In_opt_ const HINSTANCE hPrevInstance, _In_ const LPCTSTR lpCmdLine, _In_ const int nShowCmd)
+int WINAPI _tWinMain(_In_ const HINSTANCE hInstance, _In_opt_ const HINSTANCE hPrevInstance, _In_ const LPCSTR lpCmdLine, _In_ const int nShowCmd)
 {
     MessageBox(NULL, _T("Hello World"), _T("$project"), MB_OK | MB_ICONINFORMATION);
     return 0;
